361c3b5d282d6f16c1e060e292ce95cea6148985
Lauren Birts <birtslauren@gmail.com>
2020-03-31 15:32:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,12 +40,12 @@ def hit?(card_total)
   prompt_user
   get_user_input
   return card_total
-  if get_user_input == "s"
-    card_total
-    elsif get_user_input == "h"
+  if get_user_input == "h"
     deal_card
-    else 
-      invalid_command
+  elsif get_user_input == "s"
+    card_total
+  else 
+    invalid_command
 #     prompt_user
   end
 end
